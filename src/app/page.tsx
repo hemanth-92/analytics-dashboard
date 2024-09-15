b069ca17626fb8268e60d5d54bf17c425209efd4
hemanth-92 <131354764+hemanth-92@users.noreply.github.com>
2024-09-15 17:14:29 +0530

    feat:user data card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,31 @@
-import { DashboardCard } from "@/components/dashboard-card";
-import { Calendar, DollarSign, PersonStanding, UserPlus } from "lucide-react";
+import { DashboardCard, DashboardCardContent } from "@/components/dashboard-card";
+import UserDataCard, { UserDataProps } from "@/components/User-data-card";
+import { Calendar, DollarSign, PersonStanding, UserPlus, UserRoundCheck } from "lucide-react";
+import { db } from "@/lib/db";
+import { formatDistanceToNow } from "date-fns";
 
-export default function Home() {
+export default async function DashBoard() {
+
+  const recentUsers = await db.user.findMany({
+    orderBy: {
+      createdAt:'desc'
+    }, 
+    take: 8
+  });
+
+  const UserData: UserDataProps[] = recentUsers.map((account: { name: any; email: any; image: any; createdAt: string | number | Date; }) => ({
+    name: account.name || 'unknown',
+    email: account.email || 'unknow',
+    image: account.image || './mesh.jpg',
+    time : formatDistanceToNow(new Date(account.createdAt),{addSuffix:true})
+  }))
+  
   return (
     <div className="flex w-full flex-col gap-5">
       <h1 className="mx-6 text-center text-2xl font-bold">DashBoard</h1>
       <div className="container mx-auto py-8">
         <div className="flex w-full flex-col gap-5">
-          <section className="sm:grid-cols-2 xl:grid-cols-4 grid w-full grid-cols-1 gap-x-4 transition-all">
+          <section className="grid w-full grid-cols-1 gap-x-4 transition-all sm:grid-cols-2 xl:grid-cols-4">
             <DashboardCard
               label={"Total Revenue"}
               Icon={DollarSign}
@@ -32,6 +50,23 @@ export default function Home() {
               amount={43545}
               description="This Month"
             />
+          </section>
+          <section className="grid grid-cols-1 gap-4 transition-all lg:grid-cols-2">
+            <DashboardCardContent>
+              <section className="flex justify-between gap-2 pb-2">
+                <p>Recent Users</p>
+                <UserRoundCheck className="h-4 w-4" />
+              </section>
+              {UserData.map((data, index) => (
+                <UserDataCard
+                  key={index}
+                  name={data.name}
+                  email={data.email}
+                  image={data.image}
+                  time={data.time}
+                />
+              ))}
+            </DashboardCardContent>
           </section>
         </div>
       </div>
---
+++ src/components/User-data-card.tsx
@@ -0,0 +1,31 @@
+export type UserDataProps = {
+  name: string
+  email: string
+  image: any
+  time:string
+}
+export default function UserDataCard(props: UserDataProps) {
+  const defaultImage = "./mesh.jpg";
+  return (
+    <div className="flex flex-wrap justify-between gap-3">
+      <section className="flex justify-between gap-3">
+        <div className="h-12 w-12 rounded-full">
+          <img
+            width={300}
+            height={300}
+            src={props.image || defaultImage}
+            alt="avatar"
+            className="h-12 w-12 rounded-full"
+          />
+        </div>
+        <div className="text-sm">
+          <p>{props.name}</p>
+          <div className="w-[120px] overflow-hidden text-ellipsis whitespace-nowrap opacity-30 sm:w-auto">
+            {props.email}
+          </div>
+        </div>
+      </section>
+      <p className="text-sm">{ props.time}</p>
+    </div>
+  );
+}
